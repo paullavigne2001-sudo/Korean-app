cdbc1eef9eacc20990f9228ddedf16e18561bfad
paullavigne2001-sudo <paullavigne2001@gmail.com>
2026-04-09 19:00:18 +0200

    Create lessons.js

---
+++ src/data/lessons.js
@@ -0,0 +1,21 @@
+export const LESSONS = [
+  {
+    id: "greetings",
+    title: "Salutations",
+    emoji: "👋",
+    words: [
+      { korean: "안녕하세요", romanized: "annyeonghaseyo", french: "bonjour" },
+      { korean: "감사합니다", romanized: "gamsahamnida", french: "merci" },
+      { korean: "죄송합니다", romanized: "joesonghamnida", french: "désolé" }
+    ]
+  },
+  {
+    id: "food",
+    title: "Nourriture",
+    emoji: "🍜",
+    words: [
+      { korean: "밥", romanized: "bap", french: "riz" },
+      { korean: "물", romanized: "mul", french: "eau" }
+    ]
+  }
+];
